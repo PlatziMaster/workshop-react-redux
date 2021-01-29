f27b389d6418d2aa335228ae51bd168c2463762d
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-28 23:06:16 -0500

    feat: alias imports added to components; trailing spaces corrected

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import '../styles/ButtomStyles.css';
+import '@styles/ButtomStyles.css';
 
 const Buttom = ({text, onClick}) => {
     return (
-        <button 
-        className='button-st'
-        onClick={onClick}
+      <button
+      className='button-st'
+      onClick={onClick}
       >{text}</button>
     )
 }
--- src/components/CloudAnimation.jsx
+++ src/components/CloudAnimation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../styles/CloudAnimation.css';
+import '@styles/CloudAnimation.css';
 
 const CloudAnimation= () => {
     return (
