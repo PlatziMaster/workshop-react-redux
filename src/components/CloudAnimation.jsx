0f1c3aa0ef4841b7691f26034bb33d76f99cf2f6
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-28 22:55:56 -0500

    fix(CloudAnimation): className attribute fixed

--- src/components/CloudAnimation.jsx
+++ src/components/CloudAnimation.jsx
@@ -5,14 +5,14 @@ import '../styles/CloudAnimation.css';
 const CloudAnimation= () => {
     return (
         <div>
-            <div class="Cloud1"></div>
-            <div class="Cloud2"></div>
-            <div class="Cloud3"></div>
-            <div class="Cloud4"></div>
-            <div class="Cloud5"></div>
-            <div class="Cloud6"></div>
-            <div class="Cloud7"></div>
-            <div class="Cloud8"></div>
+            <div className="Cloud1"></div>
+            <div className="Cloud2"></div>
+            <div className="Cloud3"></div>
+            <div className="Cloud4"></div>
+            <div className="Cloud5"></div>
+            <div className="Cloud6"></div>
+            <div className="Cloud7"></div>
+            <div className="Cloud8"></div>
         </div>
     )
 }
